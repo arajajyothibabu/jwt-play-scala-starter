f4deb7f55109e6143294b086ae1358df074e15c5
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-05-31 01:35:35 +0530

    auth request wrapper

--- app/controllers/AuthenticatedRequest.scala
+++ app/controllers/AuthenticatedRequest.scala
@@ -1,8 +1,32 @@
 package controllers
 
+import models.UserInfo
+import play.api.mvc.Security.AuthenticatedBuilder
+import play.api.mvc._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import utils.Helpers._
+
 /**
   * Created by jyothi on 31/5/17.
   */
-class AuthenticatedRequest {
+class AuthenticatedRequest[A](val user: UserInfo, request: Request[A]) extends WrappedRequest[A](request)
+
+object AuthenticatedAction extends ActionBuilder[Request] {
+
+  def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+    val userInfoFromToken = Auth.userInfoFromToken(request.headers.apply("WWW-Authenticate"))
+    if(userInfoFromToken.isSuccess){
+      val userInfoOpt = userInfoFromToken.get.validate[UserInfo].asOpt
+      if(userInfoOpt.isDefined){
+        block(new AuthenticatedRequest(userInfoOpt.get, request))
+      }else{
+        Future.successful(Results.BadRequest("BadRequest..!"))
+      }
+    }else{
+      Future.successful(Results.Unauthorized("UnAuthorized..!"))
+    }
+  }
 
 }
